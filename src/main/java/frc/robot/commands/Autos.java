0f2e008def65abf3946686dc86b27833e5cad669
realleo245 <leolian2023@outlook.com>
2024-10-17 16:16:33 -0700

    Makes a working factory method!!!

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,21 +5,27 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveDrive;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoLoop;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
-  public Command ampToSource(AutoFactory factory) {
+  public Command ampToSource(AutoFactory factory, SwerveDrive swerveDrive) {
     AutoLoop loop = new AutoLoop("AmpToSource");
 
     AutoTrajectory trajectory = factory.trajectory("AmpToSource", loop);
+
+    loop.enabled()
+      .onTrue(new InstantCommand(() -> swerveDrive.resetPose(trajectory.getInitialPose().get()))
+      .andThen(trajectory.cmd()));
 
     return loop.cmd();
 
